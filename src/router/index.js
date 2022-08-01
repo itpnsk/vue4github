f1bc8153135183a41f311fb433fb0dcdb48a5860
Andrej Oravec <andrej@oravec.sk>
2022-08-01 11:49:06 +0200

    first

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '@/views/EventList.vue'
+import EventDetails from '@/views/EventDetails.vue'
+import About from '@/views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    // :id je placeholder ktory bude nahradeny id
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-if="event.title != null">{{ event.title }}</h1>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  props: {
+    id: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      event: {},
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+        console.log(this.event)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
